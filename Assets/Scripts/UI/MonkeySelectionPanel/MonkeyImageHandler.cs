02d4c9328dae663fa7d3b7a7d5e4cb9829d213d5
Parth Vaidya <zeusparth99@gmail.com>
2025-01-16 15:11:28 +0530

    Revert "Implemented Drag & Drop functionality"
    This reverts commit a242aad2fe49a2ce701d627b660cbb057aedb0c8.

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,14 +6,11 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour, IDragHandler , IEndDragHandler , IPointerDownHandler
+    public class MonkeyImageHandler : MonoBehaviour
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
-        private RectTransform rectTransform;
-        private Vector3 originalPosition;
-        private Vector3 originalAnchoredPosition;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -21,40 +18,10 @@ namespace ServiceLocator.UI
             this.owner = owner;
         }
 
-       
-
         private void Awake()
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
-            rectTransform = GetComponent<RectTransform>();
-            originalPosition = rectTransform.position;
-            originalAnchoredPosition = rectTransform.anchoredPosition;
-        }
-        public void OnDrag(PointerEventData eventData)
-        {
-            rectTransform.anchoredPosition += eventData.delta;
-            owner.MonkeyDraggedAt(rectTransform.position);
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            ResetMonkey();
-            owner.MonkeyDroppedAt(eventData.position);
-        }
-
-        private void ResetMonkey()
-        {
-            monkeyImage.color = new Color(1, 1, 1, 1f);
-            rectTransform.position = originalPosition;
-            rectTransform.anchoredPosition = originalAnchoredPosition;
-            GetComponent<LayoutElement>().enabled = false;
-            GetComponent<LayoutElement>().enabled = true;
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            monkeyImage.color = new Color(1, 1, 1, 0.6f);
         }
     }
 }
