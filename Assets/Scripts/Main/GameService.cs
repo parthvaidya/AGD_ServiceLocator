179b1a2fdadcc9e272117062c8b1a95a8a2fcdd0
Parth Vaidya <zeusparth99@gmail.com>
2025-01-21 12:36:15 +0530

    DI in Player Service
    Use Dependency Injection to eliminate all the GameService references in Player Service.
    Pass references of all dependencies in Init() method in Player Service.
    Use the refereces of dependencies in Player Service instead of communicating with GameService.

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -34,12 +34,23 @@ namespace ServiceLocator.Main
 
         private void Start()
         {
+            CreateService();
+            InjectDependencies();
+        }
+
+        private void CreateService()
+        {
             EventService = new EventService();
             UIService.SubscribeToEvents();
             MapService = new MapService(mapScriptableObject);
             WaveService = new WaveService(waveScriptableObject);
             SoundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
             PlayerService = new PlayerService(playerScriptableObject);
+        }
+
+        private void InjectDependencies()
+        {
+            PlayerService.Init(UIService, MapService, SoundService);
         }
 
         private void Update()
